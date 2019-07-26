c7944f2a7e02efb35d4fca25dbd07ad066147e33
huilei <1430249706@qq.com>
2019-07-26 21:28:25 +0800

    ser

--- Code/惠磊/network/my_server.c
+++ Code/惠磊/network/my_server.c
@@ -6,6 +6,8 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<error.h>
+#include<stdlib.h>
+
 #include"my_recv.h"   //自定义头文件
 
 #define SERV_PORT   4507  //     服务器的端口
@@ -31,7 +33,31 @@ struct userinfo users[] = {
 	{"xl","xl"},
 	{" "," "}  //以只含一个空格的字符串作为数组的结束标志
 };
+int find_name(const char *name)
+{
+	int i;
+	if(name ==  NULL)
+	{
+		printf( "in find_name ,NULL pointer");
+		return -2;
+	}
 
+	for(i = 0;users[i].username[0] != ' ';i++)
+	{
+		if(strcmp(users[i].username,name) == 0)   return i;
+	}
+
+	return -1;
+}
+
+void send_data(int conn_fd,const char *string)
+{
+	//向 conn_fd 发送 string 
+	if(send(conn_fd,string,strlen(string),0) < 0)
+	{
+		my_err("send",__LINE__);  //my_err 函数在my_recv.h
+	}
+}
 int main( )
 {
 	int sock_fd,conn_fd;
@@ -61,7 +87,7 @@ int main( )
 	memset(&serv_addr,0,sizeof(struct sockaddr_in));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(SERV_PORT); //设置端口号
-	serv_addr.sin.s_addr = htonl(INADD_ANY);    //设置地址   INADD_ANY 是本地地址
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);    //设置地址   INADD_ANY 是本地地址
 
 	//将套接字绑定到本地端口
 	if(bind(sock_fd,(struct sockaddr*)&serv_addr,sizeof(struct sockaddr_in)) < 0)   my_err("bind",__LINE__);
@@ -83,11 +109,55 @@ int main( )
 		//创建一个子进程处理刚刚收到的连接请求,并返回连接套接字用于收发数据
 		if((pid = fork()) == 0)	//子进程
 		{
+			while(1)
+			{                              //接收到的字符保存到buf里
+				if((ret = recv(conn_fd,recv_buf,sizeof(recv_buf),0)) < 0)
+				{
+					perror("recv");
+					exit(1);
+				}
+				recv_buf[ret - 1] = '\0';  //将数据结束标志'\n' 替换成字符串结束标志;
+				if(flag_recv == USERNAME)    //接受的是用户名
+				{
+					name_num = find_name(recv_buf);   //查找用户名是否存在,存在返回该用户名的下标,不存在返回-1,出错返回-2
+					switch(name_num)
+					{
+						case -1:
+							send_data(conn_fd,"n\n");break;   //send  发送数据
+						case -2:
+							exit(1);break;
+						default:
+							send_data(conn_fd,"y\n");
+							flag_recv = PASSWORD;
+							break;
+					}
+				}
+				else if(flag_recv == PASSWORD)   //接受到的是密码
+				{
+					if(strcmp(users[name_num].password,recv_buf) == 0)
+					{
+						send_data(conn_fd,"y\n");
+						send_data(conn_fd,"welcome login my tcp server\n");
+						printf( "%s longin\n",users[name_num].username);
+						break;
+					}
+					else
+					{
+						send_data(conn_fd,"n\n");
+					}
+				}
+			}
+
+			close(sock_fd);
+			close(conn_fd);
+			exit(0);  //结束子进程
 
 		}
-	}
-	
+		else  //父进程关闭刚刚接受的连接请求,执行accept 等待其他连接请求
+		{
+			close(conn_fd);
+		}
+	}	
 
-	
-
+	return 0;
 }
