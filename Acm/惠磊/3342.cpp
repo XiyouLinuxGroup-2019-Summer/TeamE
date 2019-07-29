de695b36d0daca0d2aa25b6c750933a509fbff7f
huilei <1430249706@qq.com>
2019-07-29 19:17:55 +0800

    al

--- Acm/惠磊/3342.cpp
+++ Acm/惠磊/3342.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<algorithm>
 #include<queue>
@@ -58,6 +59,7 @@ int main( )
 					q.push(x);
 					vis[x] = 1;
 				}
+				*/
 /*				if(edge[p][i])
 				{
 				//	temp = i;
@@ -65,7 +67,7 @@ int main( )
 					if(in[i] == 0) q.push(i);
 				}
 */
-				//if(in[temp] == 0)  q.push(temp);
+/*				//if(in[temp] == 0)  q.push(temp);
 			}
 
 		}
@@ -73,4 +75,67 @@ int main( )
 		if(ans.size() == n)  cout << "YES" << endl;
 		else  cout << "NO" << endl;
 	}
+}
+
+
+*/
+
+
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<cstring>
+
+using namespace std;
+
+vector<int> v[105];
+queue<int> q;
+int indeg[105];
+int n,m;
+int cnt;
+
+int toposort()
+{
+	while(!q.empty())  q.pop();
+	for(int i = 0;i < n;i++)   if(!indeg[i]) q.push(i);
+
+	while(!q.empty())
+	{
+		int temp = q.front();
+		cnt++;
+		q.pop();
+		
+		for(int i = 0;i < v[temp].size();i++)
+		{
+			int x = v[temp][i];
+			indeg[x]--;
+			if(!indeg[x]) q.push(x);
+		}
+	}
+
+	if(cnt == n)   return 1;
+	else return 0;
+
+}
+int main( )
+{
+	while(cin >> n >> m && n)
+	{
+		cnt = 0;
+		memset(indeg,0,sizeof(indeg));
+		for(int i = 0;i < n;i++)  v[i].clear();
+
+		int a,b;
+		for(int i = 0;i < m;i++)
+		{
+			cin >> a >> b;
+			v[a].push_back(b);
+			indeg[b]++;
+		}
+
+		if(toposort())  cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
+
 }
