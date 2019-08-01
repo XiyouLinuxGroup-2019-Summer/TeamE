dfb8e4d7ddb84242bb35ab3ed3d7978832609bdf
sky <1005035266@qq.com>
2019-08-01 22:24:08 +0800

    1

---
+++ Acm/sky/summer0/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int ans ;
+        int p,q,x,y;
+        cin >> p >> q >> x >> y;
+        int bs = q/y;
+        while(1)
+        {
+
+
+        }
+    }
+    return 0;
+}
+
